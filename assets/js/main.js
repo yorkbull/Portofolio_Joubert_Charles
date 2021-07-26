7347ca78868cd55909bbd6002619bcd2f77b3a5c
joubert charles <charles.joubert@hetic.net>
2021-07-26 23:54:56 +0200

    fin

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,18 +92,65 @@ modalCloses.forEach((modalClose, i) => {
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
-
+$(document).ready(function(){
+      $('.slider').bxSlider();
+    });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+function scrollActive(){
+  const scrollY = window.pageYOffset
 
+  section.forEach(current =>{
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }else{
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+  const nav = document.getElementById('header')
+
+  if(this.scrollY >= 80) nav.classList.add('scroll-header');else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 
 /*==================== SHOW SCROLL UP ====================*/
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up')
 
+  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll');else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'fa-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-moon' : 'fa-sun'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'fa-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
